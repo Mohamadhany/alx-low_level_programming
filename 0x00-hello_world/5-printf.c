f85d50fdb6b35b64ec30983cea568a363ea60552
Mohamadhany <Mohamadhany15@gmail.com>
2022-06-30 17:51:53 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-
 /**
-
- * main - main block
-
- * Return: 0
-
+ * main - Entry point
+ *
+ * Prints a line to the commandline using printf
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-  
 {
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
